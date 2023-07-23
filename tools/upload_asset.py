974d18e4f41ee63798321f323c00f3d1cb35d8a5
hikari <94219809+metantesan@users.noreply.github.com>
2023-07-23 03:33:42 +0330

    Update upload_asset.py

--- tools/upload_asset.py
+++ tools/upload_asset.py
@@ -6,16 +6,6 @@ from os.path import isfile, join
 email=os.getenv("email")
 password=os.getenv("password")
 mega = Mega()
-mega._login_user(email,password)
-                 
-def absoluteFilePaths(directory):
-     for dirpath,_,filenames in os.walk(directory):
-       for f in filenames:
-           yield os.path.abspath(os.path.join(dirpath, f))
-
-directory = "out/"
-file_path_generator = absoluteFilePaths(directory)
-
-Folder = mega.find('parchlinux') #change it with the folder in your mega
-for file_path in file_path_generator:
-    mega.upload(file_path, Folder[0])
+m = mega.login(email, password)
+filename = "data.csv"
+m.upload(filename)
